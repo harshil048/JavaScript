bc820a6d749c3b9b3cf63a08cd7a44fc5dbd3056
Harshil Bambhroliya <74892092+harshil048@users.noreply.github.com>
2023-08-27 12:46:59 +0000

    Data Types done

---
+++ 01/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(5+4); // we are using nodejs, not browser
+
+let name = "harshil";
+let age  = 20;
+let f = false;
+let num = null;
+num = num + 10;
+console.log(num); // output -> 10
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof 10);
+console.log(typeof "undefined");
+console.log(typeof null); // object
