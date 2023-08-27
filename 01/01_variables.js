5550f52ca08ef7bbf5c5f101a952c4ce5bbd2ee2
Harshil Bambhroliya <74892092+harshil048@users.noreply.github.com>
2023-08-27 12:29:30 +0000

    variables done

---
+++ 01/01_variables.js
@@ -0,0 +1,14 @@
+const id = 10000;
+let email = "harshil@gmail.com";
+var password = "12345";
+city = "surat";
+
+// id = 23;
+console.log(id);
+
+console.log(email);
+email = "harshil";
+password = "hello";
+city = 14
+
+console.table([id,email,password,city]);
